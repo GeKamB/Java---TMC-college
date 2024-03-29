8856634a29cba32e480609585a32b44fc866b0b8
GeKamB <k.gebski1@gmail.com>
2024-03-29 19:35:08 +0000

    array => ArrayList

--- bankApp/BankApp/src/BankApp.java
+++ bankApp/BankApp/src/BankApp.java
@@ -4,60 +4,52 @@ import java.util.*;
 class BankAccount {
     private double accBalance; 
     private String accName;
-    private double [] depHistory = new double[5];
-    private double [] withdrawHistory = new double[5];
-    int dIndex = 0;
-    int wIndex = 0;
+    private ArrayList <Double> depHistory = new ArrayList<>(); 
+    private ArrayList <Double> withdrawHistory = new ArrayList<>();
+    private String currency;
     Scanner bankScanner = new Scanner(System.in);
 
-
-
-
-    BankAccount(String accName) {
+    BankAccount(String accName, String currency) {
         this.accName = accName;
         this.accBalance = 0.00;
-        
+        this.currency = currency;
     }
    void deposit() {
-    System.out.println("Welcome " + accName + " Your account balance: £" + displayBalance());
+    System.out.println("Welcome " + accName + "\t Your account balance: " + currency + displayBalance());
     System.out.println("Enter amount you would like to deposit: ");
     double sum = bankScanner.nextDouble();
     accBalance += sum;
-    depHistory[dIndex] = sum;
-    dIndex++;
-    
-    System.out.println("Your account balance after transaction: £" + displayBalance());
+    depHistory.add(sum);    
+    System.out.println("\tYour account balance after transaction: " + currency + displayBalance());
     }
     void withdraw() {
-        System.out.println("Welcome " + accName + " Your account balance: £" + displayBalance());
+        System.out.println("Welcome " + accName + "\t Your account balance: " + currency + displayBalance());
         System.out.println("Enter amount you would like to withdraw: ");
         double sum = bankScanner.nextDouble();
-        if(accBalance >= sum)
-         {   accBalance -= sum;
-            withdrawHistory[wIndex] = sum;
-            wIndex++;
-            System.out.println("Your account balance after transaction: £" + displayBalance());
-
-
-        }else System.out.println("You have not enough funds to withdraw\n");
+        if(accBalance >= sum){
+         accBalance -= sum;
+         withdrawHistory.add(sum);
+         System.out.println("\tYour account balance after transaction: " + currency + displayBalance());
+        }
+        else System.out.println("You have not enough funds to withdraw\n");
     }
     double displayBalance() {
         return accBalance;
     }
-    String getName() {
-        return accName;
-    }
+
     void operate() {
         int choice=0;
     while(choice != 6){
         choice = 0;
+        System.out.println("\n");
         System.out.println("Hello " + accName + " What you would like to do? ");
         System.out.println(" 1. display balance \n 2.Withdraw \n 3.Deposit \n 4.Deposit History\n 5.Withdraw History\n 6.Quit ");
-        System.out.println();
+        
         choice = bankScanner.nextInt();
         switch (choice) {
             case 1:
-            System.out.println(displayBalance());
+
+            System.out.println("\n" + currency + displayBalance());
                 break;
             case 2:
             withdraw();
@@ -76,24 +68,29 @@ class BankAccount {
         }
     }
     }
-    void displayArray(double arr[]) {
-        for(int i = 0; i < arr.length; i++)
-            if(arr[i] > 0)
-            System.out.println(arr[i]);
+    void displayArray(ArrayList<Double> arr ) {
+        for(double i : arr ) System.out.println(currency + i);
+                  
+            
+            
     }
+ 
+
 }
 
 public class BankApp {
    static Scanner myScanner = new Scanner(System.in);
-
+     
 
     public static void main(String[] args) throws Exception {
         
-
         
-        System.out.println("Welcome in Bank\n Type your name and open bank account in one click!");
+        System.out.println("Welcome in Bank\n Type your name and open bank account instantly!");
         String userName = myScanner.nextLine();
-        BankAccount acc = new BankAccount(userName);
+        System.out.println("choose main currency: $ or £");
+        String currency = myScanner.nextLine();
+        BankAccount acc = new BankAccount(userName, currency);
+        
         acc.operate();
  
 
